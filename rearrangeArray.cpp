46b8a0b3c1b12e4b396495f1e144ca872cd066ba
Sanyam <soodsanyam007007@gmail.com>
2023-07-29 00:41:58 +0530

    2149. Rearrange Array Elements by Sign

---
+++ rearrangeArray.cpp
@@ -0,0 +1,16 @@
+vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> positive;
+        vector<int> negative;
+        vector<int> res;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0)
+            positive.push_back(nums[i]);
+            else
+            negative.push_back(nums[i]);
+        }
+        for(int i=0;i<positive.size();i++){
+            res.push_back(positive[i]);
+            res.push_back(negative[i]);
+        }
+        return res;
+    }
