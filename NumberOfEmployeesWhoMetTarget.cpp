2ae2bbd4ea2576b53fb2476867ad6b30e054c3bd
Sanyam <soodsanyam007007@gmail.com>
2023-07-31 09:31:45 +0530

    2798. Number of Employees Who Met the Target

---
+++ NumberOfEmployeesWhoMetTarget.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int res=0;
+        for(int i=0;i<hours.size();i++){
+            if(hours[i]>=target)
+                res++;
+        }
+        return res;
+    }
+};
